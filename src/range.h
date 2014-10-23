2792ac8ab0bd9cbffac770dca64d6784fa8f2b4b
Sebastian Freundt <freundt@ga-group.nl>
2014-10-23 12:00:12 +0000

    minor, import initial concept of ranges

---
+++ src/range.h
@@ -0,0 +1,95 @@
+/*** range.h -- some notion of intervals
+ *
+ * Copyright (C) 2012-2014 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of mutti.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if !defined INCLUDED_range_h_
+#define INCLUDED_range_h_
+#include <stdbool.h>
+#include "instant.h"
+
+typedef struct {
+	echs_instant_t from;
+	echs_instant_t till;
+} echs_range_t;
+
+
+/**
+ * Sort an array IN of NIN elements stable and in-place. */
+extern void echs_range_sort(echs_range_t *restrict in, size_t nin);
+
+
+#define ECHS_UNTIL_CHANGED	ECHS_END_OF_TIME
+#define ECHS_FOREVER		ECHS_END_OF_TIME
+#define ECHS_EMPTY_RANGE	((echs_range_t){ECHS_FOREVER, ECHS_FOREVER})
+#define ECHS_NUL_RANGE		((echs_range_t){ECHS_NUL_INSTANT, ECHS_NUL_INSTANT})
+#define ECHS_ETERNAL_RANGE	((echs_range_t){ECHS_MIN_INSTANT, ECHS_MAX_INSTANT})
+
+static inline __attribute__((pure, const)) echs_range_t
+echs_nul_range(void)
+{
+	return ECHS_NUL_RANGE;
+}
+
+static inline __attribute__((pure, const)) bool
+echs_nul_range_p(echs_range_t r)
+{
+	return echs_nul_instant_p(r.from) && echs_nul_instant_p(r.till);
+}
+
+static inline __attribute__((pure, const)) echs_range_t
+echs_empty_range(void)
+{
+	return ECHS_EMPTY_RANGE;
+}
+
+static inline __attribute__((pure, const)) bool
+echs_empty_range_p(echs_range_t r)
+{
+	return echs_instant_le_p(r.till, r.from);
+}
+
+static inline __attribute__((pure, const)) echs_range_t
+echs_eternal_range(void)
+{
+	return ECHS_ETERNAL_RANGE;
+}
+
+static inline __attribute__((pure, const)) bool
+echs_eternal_range_p(echs_range_t r)
+{
+	return (r.till.u - r.from.u) == -1ULL;
+}
+
+#endif	/* INCLUDED_range_h_ */
