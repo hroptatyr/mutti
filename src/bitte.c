832f08979ae865a19ade55f59bcf1aef2ec733d8
Sebastian Freundt <freundt@ga-group.nl>
2014-10-30 10:53:44 +0000

    major, kick bitte.c and make timeline.c the new bitte.c

--- src/bitte.c
+++ src/bitte.c
@@ -1,4 +1,4 @@
-/*** bitte.c -- bitemporal api
+/*** bitte.c -- bitemporal API using a timeline index
  *
  * Copyright (C) 2014 Sebastian Freundt
  *
@@ -37,154 +37,200 @@
 #if defined HAVE_CONFIG_H
 # include "config.h"
 #endif	/* HAVE_CONFIG_H */
-#include <stdlib.h>
+#include <sys/mman.h>
 #include <string.h>
-#include <assert.h>
 #include "bitte.h"
 #include "nifty.h"
+
+/* at the moment we operate on 4k block sizes */
+#define BLKZ	(4096U)
+/* number of transaction stamps per block */
+#define NTPB	(BLKZ / sizeof(*trans))
+
+#if !defined MAP_ANON && defined MAP_ANONYMOUS
+# define MAP_ANON	MAP_ANONYMOUS
+#elif !defined MAP_ANON
+# define MAP_ANON	(0x1000U)
+#endif	/* !MAP_ANON */
+#define PROT_RW		(PROT_READ | PROT_WRITE)
+#define MAP_MEM		(MAP_PRIVATE | MAP_ANON)
+
+struct ioff_s {
+	size_t nfacts;
+	mut_oid_t *facts;
+	size_t *offs;
+};
+
+static size_t ntrans;
+static echs_instant_t *trans;
+static mut_oid_t *facts;
+static echs_range_t *valids;
+static struct ioff_s live;
 
 #define FACT_NOT_FOUND		((size_t)-1)
 #define FACT_NOT_FOUND_P(x)	(!~(size_t)(x))
 
-/* try SoA */
-static size_t nfacts;
-static mut_oid_t *facts;
-static echs_range_t **valids;
-static echs_instant_t **transs;
-static size_t *ntrans;
+#define ECHS_RANGE_FROM(x)	((echs_range_t){x, ECHS_UNTIL_CHANGED})
 
 
-static void*
-recalloc(void *ptr, size_t old, size_t new, size_t membz)
+static inline int
+xfree(void *restrict p, size_t z)
 {
-	void *res = realloc(ptr, new * membz);
-	memset((char*)res + old * membz, 0, (new - old) * membz);
-	return res;
+	return munmap(p, z);
 }
 
-static size_t
-_get_fact(mut_oid_t fact)
+static inline void*
+xralloc(void *restrict p_old, size_t z_old, size_t z_new)
 {
-	for (size_t i = 0U; i < nfacts; i++) {
-		if (facts[i] == fact) {
+/* Like realloc() but memmove the old stuff to the end of the array,
+ * i.e. alloc (z_new - z_old) new bytes at the beginning.
+ * Unlike realloc() this can't downsize, z_new >= z_old is implied. */
+	void *p_new = mmap(p_old, z_new, PROT_RW, MAP_MEM, -1, 0);
+
+	if (UNLIKELY(p_new == MAP_FAILED)) {
+		return NULL;
+	} else if (LIKELY(p_old != NULL)) {
+		memcpy((char*)p_new + (z_new - z_old), p_old, z_old);
+		/* munmap the old guy */
+		xfree(p_old, z_old);
+	}
+	return p_new;
+}
+
+static inline void*
+xzralloc(void *restrict p_old, size_t n_old, size_t n_new, size_t z_memb)
+{
+	return xralloc(p_old, n_old * z_memb, n_new * z_memb);
+}
+
+static inline void*
+xfalloc(void *restrict p_old, size_t z_old, size_t z_new)
+{
+/* Like realloc().
+ * Unlike realloc() this can't downsize, z_new >= z_old is implied. */
+	void *p_new = mmap(p_old, z_new, PROT_RW, MAP_MEM, -1, 0);
+
+	if (UNLIKELY(p_new == MAP_FAILED)) {
+		return NULL;
+	} else if (LIKELY(p_old != NULL)) {
+		memcpy(p_new, p_old, z_old);
+		/* munmap the old guy */
+		xfree(p_old, z_old);
+	}
+	return p_new;
+}
+
+static inline void*
+xzfalloc(void *restrict p_old, size_t n_old, size_t n_new, size_t z_memb)
+{
+	return xfalloc(p_old, n_old * z_memb, n_new * z_memb);
+}
+
+
+static size_t
+_get_ioff(struct ioff_s v, mut_oid_t fact)
+{
+	size_t i_fr = FACT_NOT_FOUND;
+	size_t i;
+
+	for (i = 0U; i < v.nfacts; i++) {
+		if (v.facts[i] == MUT_NUL_OID) {
+			i_fr = i;
+			break;
+		} else if (v.facts[i] == fact) {
 			return i;
 		}
 	}
-	return FACT_NOT_FOUND;
+	for (; i < v.nfacts; i++) {
+		if (v.facts[i] == fact) {
+			return i;
+		}
+	}
+	return i_fr;
+}
+
+static int
+_put_ioff(struct ioff_s *tgt, mut_oid_t fact, size_t last)
+{
+	size_t i;
+
+	i = _get_ioff(*tgt, fact);
+	if (!FACT_NOT_FOUND_P(i) && tgt->facts[i] != MUT_NUL_OID) {
+		/* just update him then */
+		goto up_and_out;
+	}
+	/* we'll have to extend the list of live facts */
+	if (UNLIKELY((tgt->nfacts % NTPB) == 0U)) {
+		const size_t ol = tgt->nfacts;
+		const size_t nu = ol + NTPB;
+		void *pi = xzfalloc(tgt->facts, ol, nu, sizeof(*tgt->facts));
+		void *po = xzfalloc(tgt->offs, ol, nu, sizeof(*tgt->offs));
+
+		if (UNLIKELY(pi == NULL || po == NULL)) {
+			/* brill */
+			return -1;
+		}
+		tgt->facts = pi;
+		tgt->offs = po;
+	}
+	/* just ass our fact */
+	i = tgt->nfacts++;
+	tgt->facts[i] = fact;
+up_and_out:
+	tgt->offs[i] = last;
+	return 0;
 }
 
 
-echs_range_t
-bitte_valid(mut_oid_t fact, echs_instant_t as_of)
+/* meta */
+static echs_bitmp_t
+_bitte_get_as_of_now(mut_oid_t fact)
 {
-	/* check that we've got FACT already */
-	size_t i = _get_fact(fact);
+	size_t i;
 
-	if (FACT_NOT_FOUND_P(i)) {
-		/* nothing to be done then */
-		return ECHS_NUL_RANGE;
-	}
-
-	with (size_t j) {
-		const size_t ntr = ntrans[i];
-		const echs_instant_t *tr = transs[i];
-
-		for (j = 0U; j < ntr && echs_instant_le_p(tr[j], as_of); j++);
-		if (UNLIKELY(!j--)) {
-			break;
-		}
-		/* otherwise we must have a trans [j, j + 1) */
-		return valids[i][j];
-	}
-	return ECHS_EMPTY_RANGE;
-}
-
-echs_range_t
-bitte_trans(mut_oid_t fact, echs_instant_t as_of)
-{
-	/* check that we've got FACT already */
-	size_t i = _get_fact(fact);
-
-	if (FACT_NOT_FOUND_P(i)) {
-		/* nothing to be done then */
-		return ECHS_NUL_RANGE;
-	}
-
-	with (size_t j) {
-		const size_t ntr = ntrans[i];
-		const echs_instant_t *tr = transs[i];
-
-		for (j = 0U; j < ntr && echs_instant_le_p(tr[j], as_of); j++);
-		if (UNLIKELY(!j)) {
-			break;
-		}
-		/* otherwise we must have a trans [j - 1, j) */
-		return (echs_range_t){
-			tr[j - 1U],
-			j < ntr ? tr[j] : ECHS_UNTIL_CHANGED
-		};
-	}
-	return ECHS_EMPTY_RANGE;
-}
-
-echs_bitmp_t
-bitte_get(mut_oid_t fact, echs_instant_t as_of)
-{
-	/* check that we've got FACT already */
-	size_t i = _get_fact(fact);
-
-	if (FACT_NOT_FOUND_P(i)) {
-		/* nothing to be done then */
+	i = _get_ioff(live, fact);
+	if (FACT_NOT_FOUND_P(i) || live.facts[i] == MUT_NUL_OID) {
+		/* must be dead */
 		return ECHS_NUL_BITMP;
 	}
-
-	with (size_t j) {
-		const size_t ntr = ntrans[i];
-		const echs_instant_t *tr = transs[i];
-
-		for (j = 0U; j < ntr && echs_instant_le_p(tr[j], as_of); j++);
-		if (UNLIKELY(!j)) {
-			break;
-		}
-		/* otherwise we must have a trans [j - 1, j) */
-		return (echs_bitmp_t){
-			valids[i][j - 1U],
-			(echs_range_t){
-				tr[j - 1],
-				j < ntr ? tr[j] : ECHS_UNTIL_CHANGED}
-		};
-	}
-	return ECHS_EMPTY_BITMP;
+	/* yay, dead or alive, it's in our books */
+	i = live.offs[i];
+	return (echs_bitmp_t){
+		valids[i],
+		(echs_range_t){trans[i], ECHS_UNTIL_CHANGED}
+	};
 }
 
+
 int
 bitte_put(mut_oid_t fact, echs_range_t valid)
 {
-	/* check if we've got FACT already */
-	size_t i = _get_fact(fact);
+	if (UNLIKELY(!(ntrans % NTPB))) {
+		const size_t znu = ntrans + NTPB;
+		void *nu_t, *nu_i, *nu_v;
 
-	if (FACT_NOT_FOUND_P(i)) {
-		if ((nfacts % 512U) == 0U) {
-			const size_t nu = nfacts + 512U;
-			facts = realloc(facts, nu * sizeof(*facts));
-			valids = recalloc(valids, nfacts, nu, sizeof(*valids));
-			transs = recalloc(transs, nfacts, nu, sizeof(*transs));
-			ntrans = recalloc(ntrans, nfacts, nu, sizeof(*ntrans));
+		nu_t = xzfalloc(trans, ntrans, znu, sizeof(*trans));
+		nu_i = xzfalloc(facts, ntrans, znu, sizeof(*facts));
+		nu_v = xzfalloc(valids, ntrans, znu, sizeof(*valids));
+
+		if (UNLIKELY(nu_t == NULL || nu_i == NULL || nu_v == NULL)) {
+			/* try proper munmapping? */
+			return -1;
 		}
-		/* assign new fact */
-		i = nfacts++;
-		facts[i] = fact;
+		/* otherwise reassign */
+		trans = nu_t;
+		facts = nu_i;
+		valids = nu_v;
 	}
-	if ((ntrans[i] % 16U) == 0U) {
-		const size_t nu = ntrans[i] + 16U;
-		valids[i] = realloc(valids[i], nu * sizeof(*valids[i]));
-		transs[i] = realloc(transs[i], nu * sizeof(*transs[i]));
-	}
-	with (size_t it = ntrans[i]++) {
-		/* stamp off */
-		transs[i][it] = echs_now();
-		/* and bang the valid time interval */
-		valids[i][it] = valid;
+	/* stamp off then */
+	with (echs_instant_t t = echs_now()) {
+		const size_t it = ntrans++;
+
+		trans[it] = t;
+		facts[it] = fact;
+		valids[it] = valid;
+
+		_put_ioff(&live, fact, it);
 	}
 	return 0;
 }
@@ -192,27 +238,177 @@ bitte_put(mut_oid_t fact, echs_range_t valid)
 int
 bitte_rem(mut_oid_t fact)
 {
-/* this does nothing but to stamp off the last trans instant
- * nothing will be removed */
-	/* check that we've got FACT already */
-	size_t i = _get_fact(fact);
+	const size_t i = _get_ioff(live, fact);
 
-	if (FACT_NOT_FOUND_P(i)) {
-		/* nothing to be done then */
+	if (FACT_NOT_FOUND_P(i) || live.facts[i] == MUT_NUL_OID) {
+		/* he's dead already */
+		return -1;
+	} else if (echs_nul_range_p(valids[live.offs[i]])) {
+		/* dead already, just */
 		return -1;
 	}
-	if ((ntrans[i] % 16U) == 0U) {
-		const size_t nu = ntrans[i] + 16U;
-		valids[i] = realloc(valids[i], nu * sizeof(*valids[i]));
-		transs[i] = realloc(transs[i], nu * sizeof(*transs[i]));
+
+	/* otherwise kill him */
+	if (UNLIKELY(!(ntrans % NTPB))) {
+		const size_t znu = ntrans + NTPB;
+		void *nu_t, *nu_i, *nu_v;
+
+		nu_t = xzfalloc(trans, ntrans, znu, sizeof(*trans));
+		nu_i = xzfalloc(facts, ntrans, znu, sizeof(*facts));
+		nu_v = xzfalloc(valids, ntrans, znu, sizeof(*valids));
+
+		if (UNLIKELY(nu_t == NULL || nu_i == NULL || nu_v == NULL)) {
+			/* try proper munmapping? */
+			return -1;
+		}
+		/* otherwise reassign */
+		trans = nu_t;
+		facts = nu_i;
+		valids = nu_v;
 	}
-	with (size_t it = ntrans[i]++) {
-		/* stamp off */
-		transs[i][it] = echs_now();
-		/* invalidate the time interval before hand */
-		valids[i][it] = echs_empty_range();
+	/* stamp him off */
+	with (echs_instant_t t = echs_now()) {
+		const size_t it = ntrans++;
+
+		trans[it] = t;
+		facts[it] = fact;
+		valids[it] = echs_nul_range();
+
+		_put_ioff(&live, fact, it);
 	}
 	return 0;
+}
+
+echs_bitmp_t
+bitte_get(mut_oid_t fact, echs_instant_t as_of)
+{
+	size_t i_last_before = FACT_NOT_FOUND;
+	size_t i_first_after = FACT_NOT_FOUND;
+
+	if (UNLIKELY(!ntrans)) {
+		/* no transactions in this store, trivial*/
+		return ECHS_NUL_BITMP;
+	}
+	/* if AS_OF is >= the stamp of the last transaction, just use
+	 * the live table. */
+	else if (echs_instant_le_p(trans[ntrans - 1U], as_of)) {
+		return _bitte_get_as_of_now(fact);
+	}
+	/* otherwise proceed to scan */
+	with (size_t i = 0U) {
+		for (; i < ntrans && echs_instant_le_p(trans[i], as_of); i++) {
+			if (facts[i] == fact) {
+				i_last_before = i;
+			}
+		}
+		/* now I_LAST_BEFORE should hold FACT_NOT_FOUND or the index of
+		 * the last fiddle with FACT before AS_OF */
+		if (FACT_NOT_FOUND_P(i_last_before)) {
+			/* must be dead */
+			return ECHS_NUL_BITMP;
+		}
+		/* keep scanning, because the fact might have been superseded by
+		 * a more recent transaction */
+		for (; i < ntrans; i++) {
+			if (facts[i] == fact) {
+				i_first_after = i;
+				break;
+			}
+		}
+		/* now I_FIRST_AFTER should hold FACT_NOT_FOUND or the index of
+		 * the next fiddle with FACT on or after AS_OF */
+		if (FACT_NOT_FOUND_P(i_first_after)) {
+			/* must be open-ended */
+			return (echs_bitmp_t){
+				valids[i_last_before],
+				ECHS_RANGE_FROM(trans[i_last_before])
+			};
+		}
+	}
+	/* yay, dead or alive, it's in our books */
+	/* otherwise it's bounded by trans[I_FIRST_AFTER] */
+	return (echs_bitmp_t){
+		valids[i_last_before],
+		(echs_range_t){trans[i_last_before], trans[i_first_after]}
+	};
+}
+
+int
+bitte_supersede(mut_oid_t old, mut_oid_t new, echs_range_t valid)
+{
+	size_t oi;
+
+	oi = _get_ioff(live, old);
+	if (FACT_NOT_FOUND_P(oi) || live.facts[oi] == MUT_NUL_OID) {
+		/* must be dead, cannot supersede */
+		return -1;
+	}
+
+	/* otherwise make room for some (i.e. 2) insertions */
+	if (UNLIKELY(!(ntrans % NTPB) ||
+		     /* we won't need another slot if NEW is a NUL. */
+		     new != MUT_NUL_OID && (ntrans % NTPB + 1U == NTPB))) {
+		/* make sure we've got room for 2 insertions
+		 * just ask for one more and kill the lsb */
+		const size_t znu = ((ntrans + NTPB + 1U) >> 1U) << 1U;
+		void *nu_t, *nu_i, *nu_v;
+
+		nu_t = xzfalloc(trans, ntrans, znu, sizeof(*trans));
+		nu_i = xzfalloc(facts, ntrans, znu, sizeof(*facts));
+		nu_v = xzfalloc(valids, ntrans, znu, sizeof(*valids));
+
+		if (UNLIKELY(nu_t == NULL || nu_i == NULL || nu_v == NULL)) {
+			/* try proper munmapping? */
+			return -1;
+		}
+		/* otherwise reassign */
+		trans = nu_t;
+		facts = nu_i;
+		valids = nu_v;
+	}
+	/* atomically handle the supersedure */
+	with (echs_instant_t t = echs_now()) {
+		/* old guy first */
+		with (const size_t it = ntrans++) {
+			/* build new validity */
+			echs_range_t nuv = {
+				.from = valids[live.offs[oi]].from,
+				.till = valid.from,
+			};
+
+			/* bang to timeline */
+			trans[it] = t;
+			facts[it] = old;
+			valids[it] = nuv;
+
+			_put_ioff(&live, old, it);
+		}
+		/* new guy now */
+		if (new != MUT_NUL_OID) {
+			const size_t it = ntrans++;
+
+			/* bang to timeline */
+			trans[it] = t;
+			facts[it] = new;
+			valids[it] = valid;
+
+			_put_ioff(&live, new, it);
+		}
+	}
+	return 0;
+}
+
+
+echs_range_t
+bitte_valid(mut_oid_t fact, echs_instant_t as_of)
+{
+	return bitte_get(fact, as_of).valid;
+}
+
+echs_range_t
+bitte_trans(mut_oid_t fact, echs_instant_t as_of)
+{
+	return bitte_get(fact, as_of).trans;
 }
 
 /* bitte.c ends here */
